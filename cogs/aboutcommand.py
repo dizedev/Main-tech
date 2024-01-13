99e9d7e7fa50ef47fe7a14c84fdf0d2ac939f7e5
ryslandevds <87265715+ryslandevds@users.noreply.github.com>
2024-01-13 15:58:45 +0300

    Update aboutcommand.py

--- cogs/aboutcommand.py
+++ cogs/aboutcommand.py
@@ -8,9 +8,9 @@ class AboutCommand(commands.Cog):
 
     @commands.slash_command(name="бот", description="Показывает информацию о боте и их разработчиках")
     async def about(self, ctx: disnake.AppCommandInteraction):
-        embed = disnake.Embed(title='О боте', description='Многофункциональный бот.', color=0x00ff00)
-        embed.set_author(name='Hyper Tech')
-        embed.add_field(name='Автор', value='<@795901895666434070> а так же <@662994219794956298>\n')
+        embed = disnake.Embed(title='О боте', description="" color=0x00ff00)
+        embed.set_author(name='Main Tech')
+        embed.add_field(name='Автор', value='n')
         embed.add_field(name='Версия', value='v1.02.')
         embed.add_field(name='Дата выпуска', value='23.04.2023')
         await ctx.send(embed=embed)
